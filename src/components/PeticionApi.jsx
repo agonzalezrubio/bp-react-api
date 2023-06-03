e4426c773543204f13468cc5c68590937bd3903f
Alejandro <alejandro@urspos.com>
2023-06-03 17:41:18 -0500

    Added pictures & pagination

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -6,42 +6,55 @@ export const PeticionApi = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
+    
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://big-bang-theory-api.lesalvucci.workers.dev/all?page=${pageNumber}`);
+            if (response.ok) {
+                const jsonData = await response.json();
+                setData(jsonData.items);
+            } else {
+                setError('Error al obtener los datos');
+            }
+        } catch (error) {
+            setError('Error al obtener los datos: ' + error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`https://big-bang-theory-api.lesalvucci.workers.dev/all?page=${pageNumber}`);
-                if (response.ok) {
-                    const jsonData = await response.json();
-                    setData(jsonData.items);
-                } else {
-                    setError('Error al obtener los datos');
-                }
-            } catch (error) {
-                setError('Error al obtener los datos: ' + error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
         fetchData();
-    }, []);
+    }, [pageNumber]);
+
+    const handlePreviousPageClick = () => {
+        setPageNumber(pageNumber - 1);
+    }
+
+    const handleNextPageClick = () => {
+        setPageNumber(pageNumber + 1);
+    }
 
     if (loading) return <p>Loading data...</p>
     if (error) return <p>{error}</p>
-    console.log(data)
+
     return (
         <>
             <h1>Peticion API React</h1>
             <p className="read-the-docs">
                 Click the button to fetch the endpoint data
             </p>
+                <button onClick={handlePreviousPageClick} style={{marginRight: '1em'}}>Previous page</button>
+                <button onClick={handleNextPageClick}>Next page</button>
             <div className="card">
-                <ul>
-                    {data.map((item) => (
-                        <li key={item.id}>{item.name}</li>
+                <ul style={{listStyleType: 'none'}}>
+                    {data.map(({id, name, photo}) => (
+                        <li key={id}>
+                            <h4>{name}</h4>
+                            <img height="250px" src={photo} alt={`${id}pic`} />
+                        </li>
                     ))}
                 </ul>
-                {/* <button onClick={fetchData}>Fetch data</button> */}
             </div>
         </>
     )
