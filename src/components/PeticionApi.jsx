b896ad19ceeb3013e9466b25e42415f139fb77d6
Alejandro <alejandro@urspos.com>
2023-06-03 17:13:36 -0500

    Fetch working

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -3,11 +3,22 @@ import '../App.css'
 
 export const PeticionApi = () => {
     const [data, setData] = useState([]);
+    const [pageNumber, setPageNumber] = useState(1);
 
     const fetchData = async() => {
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
-            console.log(response)
+            const response = await fetch(`https://futdb.app/api/clubs?page=${pageNumber}`, { 
+                method: 'GET', 
+                headers: { 'X-Auth-Token': 'a57318c0-467b-4c2e-b761-24d8b1347ddb' }
+            });
+
+            if (response.status == 200) {
+                const json = await response.json();
+                const data = await json.items;
+                setData(data)
+            } else {
+                console.log('Error al obtener datos')
+            }
         } catch(e) {
             console.log(e)
         }
